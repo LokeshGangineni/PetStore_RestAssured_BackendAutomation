5e61877843571d6eba77f7e3502fd54553d29ac0
Bandi Prudhvi Naraya <Bandi Prudhvi Naraya@FFE-Bandi-Prudhvi-Narayana>
2025-10-27 16:08:50 +0530

    Create pet API call is successful

--- BackendApiTesting_Petstore/src/main/java/com/petStore/api/baseClass/BaseApiClass.java
+++ BackendApiTesting_Petstore/src/main/java/com/petStore/api/baseClass/BaseApiClass.java
@@ -16,19 +16,19 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class BaseApiClass {
-	DataBaseConnectivity db = new DataBaseConnectivity();
+//	DataBaseConnectivity db = new DataBaseConnectivity();
 	FileUtility fileUtil = new FileUtility();
-	static RequestSpecification reqBuild ;
-	static ResponseSpecification respBuild ;
+	public static RequestSpecification reqBuild ;
+	public static ResponseSpecification respBuild ;
 	
 	@BeforeSuite
 	public void configBS() throws IOException {
-		db.getConnectionToDataBase();
+//		db.getConnectionToDataBase();
 		
 		RequestSpecBuilder Build1 = new RequestSpecBuilder();
 		Build1.setContentType(ContentType.JSON);
 //		Build1.addHeader("", "");
-		Build1.setBaseUri(fileUtil.readDataFromPropertyFile("BaseURL"));
+		Build1.setBaseUri(fileUtil.readDataFromPropertyFile("BaseUrl"));
 		reqBuild = Build1.build();
 		
 		
@@ -40,6 +40,6 @@ public class BaseApiClass {
 	
 	@AfterSuite
 	public void configAS() throws SQLException {
-		db.closeConnectionToDataBase();
+//		db.closeConnectionToDataBase();
 	}
 }
--- BackendApiTesting_Petstore/src/test/java/Pet/EveryThingAboutPets.java
+++ BackendApiTesting_Petstore/src/test/java/Pet/EveryThingAboutPets.java
@@ -1,5 +1,59 @@
 package Pet;
 
-public class EveryThingAboutPets {
+import org.testng.annotations.Test;
 
+import com.petStore.api.PojoClass.PetPojo;
+import com.petStore.api.baseClass.BaseApiClass;
+import com.petStore.api.endpoints.IendPoints;
+import com.petStore.api.genericUtility.JsonUtility;
+
+import io.restassured.response.Response;
+
+import static  io.restassured.RestAssured.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class EveryThingAboutPets extends BaseApiClass {
+
+	JsonUtility jsonutil = new JsonUtility();
+	
+	@Test
+	public void addNewPetToStoreTest() {
+		
+		List<String> photo = Arrays.asList("https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?cs=srgb&dl=pexels-chevanon-1108099.jpg&fm=jpg");
+		PetPojo petpojo = new PetPojo("Charile",photo);
+		
+		String addPet = "{\r\n"
+				+ "  \"id\": 0,\r\n"
+				+ "  \"category\": {\r\n"
+				+ "    \"id\": 0,\r\n"
+				+ "    \"name\": \""+"Charile"+"\"\r\n"
+				+ "  },\r\n"
+				+ "  \"name\": \"doggie\",\r\n"
+				+ "  \"photoUrls\": [\r\n"
+				+ "    \"string\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"tags\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"id\": 0,\r\n"
+				+ "      \"name\": \"string\"\r\n"
+				+ "    }\r\n"
+				+ "  ],\r\n"
+				+ "  \"status\": \"available\"\r\n"
+				+ "}";
+		
+		Response resp = given()
+			.spec(reqBuild)
+			.body(petpojo)
+		.when()
+			.post(IendPoints.addnewPetToStore);
+		resp.then()
+			.spec(respBuild)
+			.log().all();
+		
+		 Object petId = jsonutil.getDataOnJsonPath(resp, ".id");
+		 System.out.println(petId);
+	}
 }
